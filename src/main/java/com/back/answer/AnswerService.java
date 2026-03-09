a250972461f9505969106c7a07a991c189cacf74
kwon15477 <kwon15477@gmail.com>
2026-03-09 17:30:43 +0900

    2-12

---
+++ src/main/java/com/back/answer/AnswerService.java
@@ -0,0 +1,23 @@
+package com.back.answer;
+
+import com.back.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
